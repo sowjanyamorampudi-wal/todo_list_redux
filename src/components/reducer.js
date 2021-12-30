5b81986955c240637f880ac307b0aecf295dda0b
sowjanya <sowjanya.morampudi@westaglielabs.com>
2021-12-30 19:20:33 +0530

    ok

---
+++ src/components/reducer.js
@@ -0,0 +1,21 @@
+const intialState = {
+  todos: [],
+};
+const reducer = (state = intialState, action) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      return {
+        ...state,
+        todos: [...state.todos, action.payload],
+      };
+    case "DELETE_TODO":
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== action.payload),
+      };
+
+    default:
+      return state;
+  }
+};
+export default reducer;
